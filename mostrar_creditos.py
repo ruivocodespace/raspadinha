c4a9472ad3e4346384764eab0b7eff4d7197b74b
Gabriel <gabrielaires.privado@gmail.com>
2025-08-26 21:43:18 -0300

    three commit

---
+++ mostrar_creditos.py
@@ -0,0 +1,9 @@
+def descontar_valor(valor_aposta):
+    global saldo
+    if saldo >= valor_aposta:
+        saldo -= valor_aposta
+        print(f"R$ {valor_aposta:.2f} descontado da aposta.")
+        return True
+    else:
+        print("Saldo insuficiente para apostar R$ {:.2f}!".format(valor_aposta))
+        return False

391499ca82f719514a2e1ef00c7f11765e3ca028
Pedro Maia <pedro_hm_caetano@estudante.sesisenai.org.br>
2025-08-27 19:35:59 -0300

    modifiquei, acrescentei premios

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -1,10 +1,17 @@
 import random
 
 def gerar_raspadinha():
+    premios = ["R$10", "R$20", "R$50", "R$100", "Nada", "R$5", "R$200"]
     print(" Raspadinha ".center(40, "-"))
     print("Número da Raspadinha: ", random.randint(1, 100))
     print("Prêmios:")
+    # Gerar 3 prêmios aleatórios para mostrar na raspadinha
+    premios_raspadinha = random.sample(premios, 3)
+    for premio in premios_raspadinha:
+        print(f"- {premio}")
     print("Raspe e Descubra:")
+    # O prêmio revelado (um dos três, aleatório)
+    print(f"-> {random.choice(premios_raspadinha)}")
     print("_______")
 
 def main():
@@ -13,5 +20,5 @@ def main():
         gerar_raspadinha()
         print("\n")
 
-
+if __name__ == "__main__":
     main()
